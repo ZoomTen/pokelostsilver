f7adb051aa647b76782be2e239100aed5dcf5d5d
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2024-05-29 21:49:28 +0700

    make the thing compile

---
+++ audio/music/feraligatr_2.asm
@@ -0,0 +1,203 @@
+Music_Feraligatr:
+	channel_count 1
+	channel 1, Music_Feraligatr_Ch1
+
+Music_Feraligatr_Ch1:
+	tempo 124
+	volume 7, 7
+	duty_cycle 1
+	pitch_offset 2
+	vibrato 18, 1, 5
+.body:
+	note_type 12, 11, 2
+	octave 3
+	note G_, 1
+	note F#, 1
+	note F_, 1
+	note E_, 1
+	note F_, 1
+	note E_, 1
+	note D#, 1
+	note D_, 1
+	note D#, 1
+	note D_, 1
+	note C#, 1
+	note C_, 1
+	note C#, 1
+	note C_, 1
+	octave 2
+	note B_, 1
+	note A#, 1
+	note B_, 1
+	note A#, 1
+	note A_, 1
+	note G#, 1
+	note A_, 1
+	note G#, 1
+	note G_, 1
+	note F#, 1
+	note G_, 1
+	note F#, 1
+	note F_, 1
+	note E_, 1
+	note F_, 1
+	note F#, 1
+	note G_, 1
+	note G#, 1
+	octave 3
+	note D_, 6
+	octave 2
+	note F_, 6
+	note A_, 4
+	note G_, 4
+	note G#, 4
+	note A_, 8
+	note A#, 6
+	note F_, 6
+	note A#, 4
+	note G_, 4
+	note A_, 4
+	volume_envelope 11, 5
+	note A#, 8
+	octave 3
+	volume_envelope 11, 2
+	note A_, 6
+	note F_, 6
+	note A_, 4
+	note G_, 4
+	note G#, 4
+	note A_, 8
+	note A#, 6
+	note F_, 6
+	note A#, 4
+	note G_, 4
+	note A_, 4
+	volume_envelope 6, -7
+	note A#, 8
+	octave 4
+.mainloop:
+	volume_envelope 11, 6
+	octave 2
+	note A_, 6
+	note F#, 6
+	note F#, 4
+	octave 3
+	note C_, 6
+	octave 2
+	note B_, 6
+	note G_, 4
+	octave 3
+	note D#, 8
+	note F_, 1
+	note E_, 1
+	note D_, 1
+	note C#, 1
+	note D_, 1
+	note D#, 1
+	note E_, 1
+	note F_, 1
+	note G_, 8
+	note F#, 1
+	note F_, 1
+	note D#, 1
+	note D_, 1
+	note D#, 1
+	note E_, 1
+	note F_, 1
+	note F#, 1
+	octave 2
+	note A_, 6
+	note F#, 6
+	note F#, 4
+	octave 3
+	note C_, 6
+	octave 2
+	note B_, 6
+	note A_, 4
+	note B_, 8
+	note A_, 8
+	note G_, 8
+	octave 3
+	note C_, 8
+	octave 2
+	note A#, 6
+	note G_, 6
+	note G_, 4
+	octave 3
+	note C#, 6
+	note C_, 6
+	octave 2
+	note G#, 4
+	octave 3
+	note E_, 8
+	note F#, 1
+	note F_, 1
+	note D#, 1
+	note D_, 1
+	note D#, 1
+	note E_, 1
+	note F_, 1
+	note F#, 1
+	note G#, 8
+	note G_, 1
+	note F#, 1
+	note E_, 1
+	note D#, 1
+	note E_, 1
+	note F_, 1
+	note F#, 1
+	note G_, 1
+	octave 2
+	note A#, 6
+	note G_, 6
+	note G_, 4
+	octave 3
+	note C#, 6
+	note C_, 6
+	octave 2
+	note G#, 4
+	octave 3
+	note C_, 8
+	octave 2
+	note A#, 8
+	note G#, 8
+	octave 3
+	note C#, 8
+	volume_envelope 11, 7
+	octave 2
+	note B_, 6
+	note A_, 6
+	note B_, 4
+	octave 3
+	note C_, 6
+	octave 2
+	note B_, 6
+	note G_, 4
+	octave 3
+	note C_, 8
+	octave 2
+	note G_, 8
+	note B_, 6
+	note A_, 6
+	octave 3
+	note C_, 4
+	note D_, 16
+	note A_, 8
+	octave 4
+	note D_, 8
+	note D_, 16
+	octave 3
+	note A_, 16
+	octave 2
+	note G_, 4
+	note A_, 4
+	note G_, 4
+	note F#, 4
+	note G_, 16
+	note A_, 4
+	note B_, 4
+	note A_, 4
+	octave 3
+	note C#, 4
+	note D_, 16
+	sound_loop 0, .mainloop
